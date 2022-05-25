d9f26a06cbc5cabe682a809048a210b5f29b990d
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-05-25 16:38:35 +0000

    Initial javascript and test code structure

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,15 +2,35 @@
  * @jest-environment jsdom
  */
 
+const {
+    game
+} = require("../game");
+
 /**
  * This can act as a boilerplate
  * This will be the same for every test file
  * And will always be the first block of code
  */
+
 beforeAll(() => {
     let fs = require("fs");
     let fileContents = fs.readFileSync("index.html", "utf-8");
     document.open();
     document.write(fileContents);
     document.close();
-})
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+});
